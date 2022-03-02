44a279f8479cd2509042c816572c265d63266bd8
CarbonIce <80366295+CarbonIce@users.noreply.github.com>
2022-03-01 19:01:49 -0800

    More enemies
    And you can now kill those enemies by shooting at them

--- Build/Hell2_27_22.framework.js
+++ Build/Hell2_27_22.framework.js
@@ -1283,7 +1283,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2205520;
+STATICTOP = STATIC_BASE + 2207600;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3241,7 +3241,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2205520;
+var STATIC_BUMP = 2207600;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
